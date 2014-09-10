b0dfc768d9237be12d7e021bd6317ec5684360cf
Gary Kertis <garykertis@PUBWS76.local>
2014-09-10 18:59:04 -0400

    Begin work on analytics

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var express = require('express')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var numUsers = 0;
+var sites = {};
 app.use(express.static(__dirname));
 app.enable('trust proxy');
 
 var options = {
-	root: __dirname
+    root: __dirname
 }
 
 app.get('/', function(req, res) {
@@ -18,12 +20,34 @@ app.get('/backend', function(req, res) {
 })
 
 io.on('connection', function(socket) {
+    ++numUsers;
+    socket.emit('userJoined', numUsers);
+
     socket.on('scrolled', function(msg) {
         io.emit('dashboard update', msg);
-        console.log(msg);
+    });
+
+    socket.on('sendUserInfo', function(site) {
+        io.emit('update info', site);
+        socket.site = site;
+        // add the client's username to the global list
+        sites[site] = site;
+        console.log(socket.site);
+    });
+
+
+    socket.on('disconnect', function() {
+        --numUsers;
+        delete sites[socket.site];
+        socket.emit('userLeft', {
+            users: numUsers,
+            site: socket.site
+        });
     });
 
 });
+
+
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
