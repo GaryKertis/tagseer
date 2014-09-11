3a57a372123097d829d3410d3d5619b396f57819
Gary Kertis <garykertis@PUBWS76.local>
2014-09-11 15:07:19 -0400

    Fix syntax error

--- pixel.js
+++ pixel.js
@@ -1,5 +1,7 @@
-var socket = io.connect('http://garykertis.com/realtime');
-socket.emit('sendUserInfo', {
-    'hosts': document.location.hostname,
-    'referrers': document.referrer == "" ? "Unknown" : document.referrer
+$(function() {
+    var socket = io.connect('http://garykertis.com/realtime');
+    socket.emit('sendUserInfo', {
+        'hosts': document.location.hostname,
+        'referrers': document.referrer == "" ? "Unknown" : document.referrer
+    });
 });
