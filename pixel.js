7f38755f821c39b992512f79903f767a9dcfb4aa
Gary Kertis <garykertis@PUBWS76.local>
2014-09-11 13:40:39 -0400

    Fix pixel issue

--- pixel.js
+++ pixel.js
@@ -1,5 +1,4 @@
 var socket = io();
-socket.emit('scrolled', isScrolledIntoView('#ad'));
 socket.emit('sendUserInfo', {
     'hosts': document.location.host.split(':')[0],
     'referrers': document.referrer == "" ? "Unknown" : document.referrer
