481e20fd2b63d9928f10f42413319c5a0784acb4
Gary Kertis <garykertis@PUBWS76.local>
2014-09-11 14:13:05 -0400

    Try again.

--- pixel.js
+++ pixel.js
@@ -1,4 +1,4 @@
-var socket = io();
+var socket = io.connect('http://garykertis.com/realtime');
 socket.emit('sendUserInfo', {
     'hosts': document.location.host.split(':')[0],
     'referrers': document.referrer == "" ? "Unknown" : document.referrer
