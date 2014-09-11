446f9cacf472062d64361f151fbd0598fe524d2d
Gary Kertis <garykertis@PUBWS76.local>
2014-09-11 15:09:38 -0400

    Remove jQuery from Pixel

--- pixel.js
+++ pixel.js
@@ -1,7 +1,7 @@
-$(function() {
+(function() {
     var socket = io.connect('http://garykertis.com/realtime');
     socket.emit('sendUserInfo', {
         'hosts': document.location.hostname,
         'referrers': document.referrer == "" ? "Unknown" : document.referrer
     });
-});
+})();
