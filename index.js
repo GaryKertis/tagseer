8a66040561599c7f7ea7e1fc1885651e656c41f0
Gary Kertis <garykertis@PUBWS76.local>
2014-09-12 12:12:01 -0400

    Try Again

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var numUsers = 0;
 var sites = {};
 
 var enableCORS = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://thelivre.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, *');
 
