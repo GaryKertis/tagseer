9d6d9c736ed0e7da92871086a8b6560a4b9ee0e7
Gary Kertis <garykertis@PUBWS76.local>
2014-09-12 11:44:03 -0400

    Add cors

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ var io = require('socket.io')(http);
 var numUsers = 0;
 var sites = {};
 
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, *');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    };
+};
+
+
+// enable CORS!
+app.use(enableCORS);
+
 app.use(express.static(__dirname));
 
 app.enable('trust proxy');
