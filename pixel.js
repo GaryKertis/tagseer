ca0c4253c1271274d77a937ccb8131ffe80975e1
Gary Kertis <garykertis@PUBWS76.local>
2014-09-12 16:57:48 -0400

    Finalized initial pixel

--- pixel.js
+++ pixel.js
@@ -1,8 +1,22 @@
 (function() {
-    var socket = io('http://garykertis.com/', {
+
+    var scriptid = document.getElementById('rtpix').src;
+
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(scriptid);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+
+    var adid = getParameterByName('adid');
+    if (typeof adid !== 'undefined') console.log(adid);
+
+    var socket = io('http://' + document.getElementById('rtpix').src.split('/')[2], {
         'multiplex': false,
         'path': '/socket.io'
     });
+
     socket.emit('sendUserInfo', {
         'hosts': document.location.hostname.split('.')[0],
         'referrers': document.referrer == "" ? "Unknown" : document.referrer
