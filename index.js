deb829a2e2bc432be6157091eb9679e003e743d8
Gary Kertis <garykertis@PUBWS76.local>
2014-09-12 12:34:42 -0400

    Try again

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var enableCORS = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://thelivre.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, *');
+    res.header('Access-Control-Allow-Credentials', 'true');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
