96f8ab9dbc93605d9baa0abd4f09eb46e9523f84
Gary Kertis <garykertis@PUBWS76.local>
2014-10-06 11:39:54 -0400

    Try moving disconnect event inside first user response event.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var external = require('http');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var allsockets = [];
 var enableCORS = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://thelivre.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -48,12 +48,12 @@ io.on('connect', function(socket) {
             if (io.sockets.sockets[sock] != backendid) io.sockets.sockets[sock].disconnect();
         }
         for (sock in io.sockets.sockets) {
-        console.log("Socket " + io.sockets.sockets[sock].id + "is connected: " + io.sockets.sockets[sock].connected)
+            console.log("Socket " + io.sockets.sockets[sock].id + "is connected: " + io.sockets.sockets[sock].connected)
         };
         console.log(new Date().toString() + "the backend connected.");
-        console.log("io.sockets.sockets.length is " + io.sockets.sockets.length-1);
+        console.log("io.sockets.sockets.length is " + io.sockets.sockets.length - 1);
         backendid.on('disconnect', function() {
-            backendid = null; 
+            backendid = null;
             console.log(new Date().toString() + "the backend disconnected.");
             for (sock in io.sockets.sockets) {
                 if (io.sockets.sockets[sock] != backendid) io.sockets.sockets[sock].disconnect();
@@ -62,10 +62,9 @@ io.on('connect', function(socket) {
     });
 
     if (backendid !== null) {
-        doSockets(); 
-    }
-    else {
-        var timeout = setTimeout(function(){
+        doSockets();
+    } else {
+        var timeout = setTimeout(function() {
             if (backendid === null) {
                 socket.disconnect();
             }
@@ -76,6 +75,7 @@ io.on('connect', function(socket) {
     function doSockets() {
 
         socket.on('sui', function(data) {
+            allsockets.push(socket.id);
             data.id = socket.id;
             //console.log(data);
 
@@ -83,63 +83,62 @@ io.on('connect', function(socket) {
 
             if (typeof ip !== "undefined") {
 
-            var options = {
-                host: 'www.telize.com',
-                port: 80,
-                path: '/geoip/' + ip
-            };
+                var options = {
+                    host: 'www.telize.com',
+                    port: 80,
+                    path: '/geoip/' + ip
+                };
 
-            external.get(options, function(res) {
-                var str = "";
+                external.get(options, function(res) {
+                    var str = "";
 
-                res.on('data', function(chunk) {
-                    str += chunk;
-                });
-
-                res.on('end', function() {
-                    //console.log(str);
-                    ipdata = JSON.parse(str);
-                    // your code here if you want to use the results !
-
-                    if (backendid !== null && socket.connected) backendid.emit('uj', {
-                        'total': io.sockets.sockets.length - 1,
-                        'id': socket.id,
-                        'latitude': ipdata.latitude || 0,
-                        'longitude': ipdata.longitude || 0,
-                        'site': data.site
+                    res.on('data', function(chunk) {
+                        str += chunk;
                     });
+
+                    res.on('end', function() {
+                        //console.log(str);
+                        ipdata = JSON.parse(str);
+                        // your code here if you want to use the results !
+
+                        if (backendid !== null && socket.connected) backendid.emit('uj', {
+                            'total': allsockets.length,
+                            'id': socket.id,
+                            'latitude': ipdata.latitude || 0,
+                            'longitude': ipdata.longitude || 0,
+                            'site': data.site
+                        });
+                    });
+
+
+                }).on('error', function(e) {
+                    console.log("Got error: " + e.message);
+                });
+            }
+
+            socket.on('disconnect', function() {
+
+                allsockets.splice(allsockets.indexOf(socket.id), 1)
+
+                if (backendid !== null) backendid.emit('ul', {
+                    susers: allsockets.length,
+                    suid: socket.id
                 });
 
-
-            }).on('error', function(e) {
-                console.log("Got error: " + e.message);
-            });
-        }
-            
-            //if (backendid !== null) backendid.emit('ui', data);
-            // add the client's username to the global list
-            //console.log(socket.uid + ' is on site ' + data.hosts);
-            for (creative in data.creatives) {
-                //console.log('          with data ' + creative);
-            }
-        });
-
-        socket.on('disconnect', function() {
-
-            if (backendid !== null) backendid.emit('ul', {
-                susers: io.sockets.sockets.length - 1,
-                suid: socket.id
+                //console.log(new Date().toString() + " the user disconnected, id #" + socket.uid);
             });
 
-            //console.log(new Date().toString() + " the user disconnected, id #" + socket.uid);
+            //console.log(new Date().toString() + " a user joined, id #" + socket.uid);
+
+            socket.on('uv', function(data) {
+                data.id = socket.id;
+                if (backendid !== null) backendid.emit('ubv', data);
+            });
+
+
         });
 
-        //console.log(new Date().toString() + " a user joined, id #" + socket.uid);
 
-        socket.on('uv', function(data) {
-            data.id = socket.id;
-            if (backendid !== null) backendid.emit('ubv', data);
-        });
 
     }
 
