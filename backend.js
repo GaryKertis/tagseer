63ae0a522eab91a0ef400f716712ecd12ba74c8c
Gary Kertis <garykertis@PUBWS76.local>
2014-10-07 18:51:17 -0400

    Add area chart.

--- backend.js
+++ backend.js
@@ -6,10 +6,14 @@
  var sitelist = [];
  var browserlist = [];
  var userData = new Object;
-
+ var currentConnected = 0;
+ var connecteduserdata = [
+     ['Minute', 'Users']
+ ];
 
  var chart;
  var browserchart;
+ var userchart;
 
  var map = new google.maps.Map(document.getElementById('map-canvas'),
      mapOptions);
@@ -32,10 +36,51 @@
  });
  google.setOnLoadCallback(drawChart);
 
+ setInterval(function() {
+     var date = new Date;
+     connecteduserdata.push([date.getTime(), currentConnected]);
+     userChart(connecteduserdata);
+ }, 5000);
+
+ function formatChartData(arr) {
+     var a = [],
+         b = [],
+         prev;
+
+     arr.sort();
+     for (var i = 0; i < arr.length; i++) {
+         if (arr[i] !== prev) {
+             a.push(arr[i]);
+             b.push(1);
+         } else {
+             b[b.length - 1]++;
+         }
+         prev = arr[i];
+     }
+
+     formatted = [a, b];
+
+     output = [
+         ['Site', 'Users']
+     ];
+
+     for (var i = 0; i < formatted[0].length; i++) {
+         output.push([formatted[0][i], formatted[1][i]]);
+     }
+     return output;
+ }
+
+
+ function updateCharts() {
+     sitesChart(formatChartData(sitelist));
+     browserChart(formatChartData(browserlist));
+ }
+
  function drawChart() {
 
      chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
      piechart = new google.visualization.PieChart(document.getElementById('piechart'));
+     userchart = new google.visualization.AreaChart(document.getElementById('userchart'));
 
      updateCharts();
 
@@ -58,6 +103,27 @@
      };
 
      piechart.draw(data, options);
+ }
+
+ function userChart(users) {
+     var data = google.visualization.arrayToDataTable(users);
+
+     var options = {
+         title: 'User Volume',
+         hAxis: {
+             textPosition: 'none'
+         },
+         vAxis: {
+             minValue: 0,
+             maxValue: 25
+         },
+         animation: {
+             duration: 500,
+             easing: 'out'
+         }
+     };
+
+     userchart.draw(data, options);
  }
 
  function sitesChart(siteData) {
@@ -101,6 +167,8 @@
 
  $(function() {
 
+     var counter = 0;
+
      var socket = io(document.location.host, {
          'multiplex': false,
          'path': '/socket.io'
@@ -110,43 +178,13 @@
 
      socket.emit('bc');
 
-     function formatChartData(arr) {
-         var a = [],
-             b = [],
-             prev;
-
-         arr.sort();
-         for (var i = 0; i < arr.length; i++) {
-             if (arr[i] !== prev) {
-                 a.push(arr[i]);
-                 b.push(1);
-             } else {
-                 b[b.length - 1]++;
-             }
-             prev = arr[i];
-         }
-
-         formatted = [a, b];
-
-         output = [
-             ['Site', 'Users']
-         ];
-
-         for (var i = 0; i < formatted[0].length; i++) {
-             output.push([formatted[0][i], formatted[1][i]]);
-         }
-         return output;
-     }
-
-     function updateCharts() {
-         sitesChart(formatChartData(sitelist));
-         browserChart(formatChartData(browserlist));
-     }
-
      socket.on('uj', function(data) {
          //console.log('A user joined with id #' + data.id + " at " + data.latitude + "," + data.longitude);
+         counter++;
 
-         $('#TotalUsers').text(data.total);
+         $("#Impressions").text(counter)
+         currentConnected = data.total || 0;
+         $('#TotalUsers').text(currentConnected);
 
          var mapOptions = {
              icon: {
@@ -170,7 +208,6 @@
              var count = 30;
 
              if (typeof data.creatives.v === "number") {
-                 console.log(data.creatives.v);
 
                  if (data.creatives.v > 0) {
                      var icon = marker.get('icon')
